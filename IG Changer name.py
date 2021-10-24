f6915efd4b273a5a9a5c7dfbda94dac338a58fac
focusq8 <31973454+focusq8@users.noreply.github.com>
2021-10-25 00:45:23 +0300

    Update IG Changer name.py

--- IG Changer name.py
+++ IG Changer name.py
@@ -1,61 +1,83 @@
-# tool to check if username in Instagram can be change or no
 import requests
-import uuid
-import random
+import json
+from uuid import uuid4
 
-print("Check 14 Days usernames in Instagram \n")
 
-random_choose = "abcdefghijklmnopqrwstuvwxyz1234567890"
-try:
-	file_username = open("users.txt", "r")
-except FileNotFoundError:
-	print ("please make file users.txt")
-	input()
+request = requests.session()
+uid = uuid4()
+username = ''
+password = ''
+name = ''
+csrf_info = ['csrftoken'] #define session id to log in your account without secure access
 
-while 1:
-	email = random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+"@gmail.com"
-	password = random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)+random.choice(random_choose)
-	
-	username = file_username.readline().split("\n")[0]
-	if username == "":
-		exit()
-	  
-	uid = str(uuid.uuid4())
+print("""       
 
-	
-	url = "https://i.instagram.com/api/v1/accounts/create_validated/"
-	
-	header_login = {
-		'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
-		"Accept": "*/*",
-		"Accept-Encoding": "gzip, deflate",
-		"Accept-Language": "en-US",
-		"X-IG-Capabilities": "3brTvw==",
-		"X-IG-Connection-Type": "WIFI",
-		"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-		'Host': 'i.instagram.com'
+Log In With Your Account Please: 
+
+        """)
+username = input("Username: ")
+password = input("Password: ")
+name = input("Name: ")     #Write Your name you want to change it
+print("Please wait.....")
+
+logurl = 'https://i.instagram.com/api/v1/accounts/login/'
+
+
+header_info = {
+    'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US",
+    "X-IG-Capabilities": "3brTvw==",
+    "X-IG-Connection-Type": "WIFI",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    'Host': 'i.instagram.com',
+    'Connection': 'keep-alive'
 }
-	data_log = {
-		"device_id": uid,
-		"uuid": uid,
-		"email": email,
-		"password": password,
-		"_csrftoken": "3IsBQzwjBN5xG242t0xPmw9vBq6tMcDo",
-		"firt_name": "",
-		"username": username
+nameurl = 'https://i.instagram.com/api/v1/accounts/set_phone_and_name/'
+namedata = {
+ 
+    "first_name": name
 }
-	request_get = requests.post(url, data=data_log, headers=header_login)
-	
-	if "username_held_by_others" in request_get.text:
-		print(f"@{username} has 14 Days you can not change it ")
-		with open("14 Days.txt", "a") as file:
-			file.write(f"{username}\n")
-	
-	elif "username_is_taken" in request_get.text:
-		print(f"@{username} has Not 14 Days you can change it now")
-		with open("Not 14 Days.txt", "a") as file:
-			file.write(f"{username}\n")
-	
-	else:
-		print(f"Error >>  @{username}")
-		print(request_get.text)
+def cookie_function():
+    global cookie_log 
+          
+    request_url = request.post(nameurl, headers=header_info, data=namedata, cookies=cookie_log).status_code
+   
+    
+    if request_url == 200:
+      print(f"Your Name is Changing To",name)
+      input()
+      print("Please Press Enter To Exit")
+    else:
+        print(f"Error Could not login into your account")
+        input()
+        exit()
+
+logdata = {
+
+    'uuid': uid,
+    'password': password,
+    'username': username,
+    'device_id': uid,
+    'from_reg': 'false',
+    '_csrftoken': csrf_info,
+    'login_attempt_countn': '0'
+}
+ 
+def login():
+    global log, loginJS
+    global cookie_log 
+    log = request.post(logurl, headers=header_info, data=logdata)
+    loginJS = log.json()
+    if 'logged_in_user' in log.text:
+        cookie_log = log.cookies
+        cookie_info = cookie_log.get_dict()
+        csrf_info = cookie_info['csrftoken']
+
+        cookie_function()
+        pass
+    else:
+        print("Error")
+        exit()
+login()
